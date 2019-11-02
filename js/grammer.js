e1c6c85a8ab0a861610a778b1d63f83ba6f5c27e
yawuplus <1397991131@qq.com>
2019-11-02 11:36:25 +0800

    add

--- js/grammer.js
+++ js/grammer.js
@@ -1,8 +1,34 @@
-//标签
-top:
-    for (var i = 0; i < 3; i++){
-        for (var j = 0; j < 3; j++){
-            if (i === 1 && j === 1) break top;
-            console.log('i=' + i + ', j=' + j);
-        }
+function print(s) {
+    console.log(s);
+}
+var print = function (s) {
+    console.log(s);
+}
+var foo= new Function(
+    'return "hello world";'
+);
+// 等同于
+function foo() {
+    return 'hello world';
+}
+//同一个函数被多次声明，后面的声明就会覆盖前面的声明。
+function f() {
+    console.log(1);
+}
+// f() // 2
+
+function f() {
+    console.log(2);
+}
+// f() // 2
+function fib(num) {
+    if(num===0){
+        return 0;
     }
+    if(num===1){
+        return 1;
+    }
+    return fib(num-2)+fib(num-1);
+}
+// console.log(fib(40));
+console.log(Math.sqrt.toString());
